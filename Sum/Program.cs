93f228fb6903cefed777a52090fb3a43e2688a16
Nikita <nikitaaalimpiev@gmail.com>
2022-11-03 10:55:58 +0600

    Add dotnet hello, Sum and gitignore

--- Sum/Program.cs
+++ Sum/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("What's your name?");
+string username = Console.ReadLine();
+Console.Write("Hi, ");
+Console.Write(username);
